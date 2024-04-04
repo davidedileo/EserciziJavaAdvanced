1f1f0d48d79e43b6ac5284d718444926d2a8518f
davidedileo <160485277+davidedileo@users.noreply.github.com>
2024-04-04 10:31:47 +0200

    Add files via upload

---
+++ operatoreternario2/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        /*String result;
+        if (value.length() >= 10) {
+            result = "Lunghezza maggiore o uguale di 10";
+        } else {
+            result = "Lunghezza minore di 10";
+        }
+        return result; */
+        return (value.length() >= 10) ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10";
+    }
+}
